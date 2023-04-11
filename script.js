9e2913e53270923942f7893e2d7eabb5df760b0c
Pit_P <105433218+PeterIsch616@users.noreply.github.com>
2023-04-11 18:43:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,106 +1,84 @@
-let input,search,pr,result,result_arr, locale_HTML, result_store;
+const inputSearch = document.querySelector("input");
+const inputContainer = document.querySelector(".dropdown-container");
+const chosens = document.querySelector(".chosens");
 
-function func() {
- 	locale_HTML = document.body.innerHTML;   // сохраняем в переменную весь body (Первоначальный)
+chosens.addEventListener("click", function(event) {
+    let target = event.target;
+    if (!target.classList.contains("btn-close")) return;
+
+    target.parentElement.remove();
+});
+
+inputContainer.addEventListener("click", function(event) {
+    let target = event.target;
+    if (!target.classList.contains("dropdown-content")) {
+	return;
+    }
+    addChosen(target);
+    inputSearch.value = "";
+    removePredictions();
+});
+
+function removePredictions() {
+    inputContainer.innerHTML = "";
 }
-setTimeout(func, 1000);  //ждем подгрузки Jsona и выполняем
 
-function FindOnPage(name, status) {
+function showPredictions(repositories) {
+    let dropdownPredictions = document.querySelectorAll(".dropdown-content");
+    removePredictions();
 
-	input = document.getElementById(name).value; //получаем значение из поля в html
-	
-	if(input.length<3&&status==true)
-	{
-		alert('Для поиска вы должны ввести три или более символов');
-		function FindOnPageBack() { document.body.innerHTML = locale_HTML; }
+    for (let repositoryIndex = 0; repositoryIndex < 5; repositoryIndex++) {
+	let name = repositories.items[repositoryIndex].name;
+	let owner = repositories.items[repositoryIndex].owner.login;
+	let stars = repositories.items[repositoryIndex].stargazers_count;
+
+	let dropdownContent = `<div class="dropdown-content" data-owner="${owner}" data-stars="${stars}">${name}</div>`;
+	inputContainer.innerHTML += dropdownContent;
+    }
+}
+
+function addChosen(target) {
+    let name = target.textContent;
+    let owner = target.dataset.owner;
+    let stars = target.dataset.stars;
+    
+    chosens.innerHTML += `<div class="chosen">Name: ${name}<br>Owner: ${owner}<br>Stars: ${stars}<button class="btn-close"></button></div>`;
+}
+
+async function getPredictions() {
+    const urlSearchRepositories = new URL("https://api.github.com/search/repositories");
+    let repositoriesPart = inputSearch.value;
+    if (repositoriesPart == "") {
+	removePredictions();
+	return;
+    }
+
+    urlSearchRepositories.searchParams.append("q", repositoriesPart)
+    try {
+	let response = await fetch(urlSearchRepositories);
+	if (response.ok) {
+	    let repositories = await response.json();
+	    showPredictions(repositories);
 	}
-	
-	if(input.length>=3)
-	{
-		function FindOnPageGo() {
-
-			search = '/'+input+'/g';  //делаем из строки регуярное выражение
-			pr = document.body.innerHTML;   // сохраняем в переменную весь body
-			result = pr.match(/>(.*?)</g);  //отсекаем все теги и получаем только текст
-			result_arr = [];   //в этом массиве будем хранить результат работы (подсветку)
-
-			let warning = true;
-			for(let i=0;i<result.length;i++) {
-				if(result[i].match(eval(search))!=null) {
-					warning = false;
-				}
-			}
-			if(warning == true) {
-				alert('Не найдено ни одного совпадения');
-			}
-
-			for(let i=0; i<result.length;i++) {
-				result_arr[i] = result[i].replace(eval(search), '<span style="background-color:yellow;">'+input+'</span>'); //находим нужные элементы, задаем стиль и сохраняем в новый массив
-			}
-			for(let i=0; i<result.length;i++) {
-				pr=pr.replace(result[i],result_arr[i])  //заменяем в переменной с html текст на новый из новогом массива
-			}
-			document.body.innerHTML = pr;  //замена html кода
-		}
-	}
-	function FindOnPageBack() { document.body.innerHTML = locale_HTML; }
-	if(status) { FindOnPageBack(); FindOnPageGo(); } //чистим прошлое и выделяем найденное
-	if(!status) { FindOnPageBack(); } //снимаем выделение
-}
-// getting all required elements
-const searchWrapper = document.querySelector(".search-input");
-const inputBox = searchWrapper.querySelector("input");
-const suggBox = searchWrapper.querySelector(".autocom-box");
-const icon = searchWrapper.querySelector(".icon");
-let linkTag = searchWrapper.querySelector("a");
-let webLink;
-// if user press any key and release
-inputBox.onkeyup = (e)=>{
-    let userData = e.target.value; //user enetered data
-    let emptyArray = [];
-    if(userData){
-        icon.onclick = ()=>{
-            webLink = `https://www.google.com/search?q=${userData}`;
-            linkTag.setAttribute("href", webLink);
-            linkTag.click();
-        }
-        emptyArray = suggestions.filter((data)=>{
-            //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
-            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
-        });
-        emptyArray = emptyArray.map((data)=>{
-            // passing return data inside li tag
-            return data = `<li>${data}</li>`;
-        });
-        searchWrapper.classList.add("active"); //show autocomplete box
-        showSuggestions(emptyArray);
-        let allList = suggBox.querySelectorAll("li");
-        for (let i = 0; i < allList.length; i++) {
-            //adding onclick attribute in all li tag
-            allList[i].setAttribute("onclick", "select(this)");
-        }
-    }else{
-        searchWrapper.classList.remove("active"); //hide autocomplete box
+	else return null;
+    } catch(error) {
+	return null;
     }
 }
-function select(element){
-    let selectData = element.textContent;
-    inputBox.value = selectData;
-    icon.onclick = ()=>{
-        webLink = `https://www.google.com/search?q=${selectData}`;
-        linkTag.setAttribute("href", webLink);
-        linkTag.click();
-    }
-    searchWrapper.classList.remove("active");
+
+function debounce(fn, timeout) {
+    let timer = null;
+
+    return (...args) => {
+	clearTimeout(timer);
+	return new Promise((resolve) => {
+	    timer = setTimeout(
+		() => resolve(fn(...args)),
+		timeout,
+	    );
+	});
+    };
 }
-function showSuggestions(list){
-    let listData;
-    if(!list.length){
-        userValue = inputBox.value;
-        listData = `<li>${userValue}</li>`;
-    }else{
-      listData = list.join('');
-    }
-    suggBox.innerHTML = listData;
-}
-}
+
+const getPredictionsDebounce = debounce(getPredictions, 500);
+inputSearch.addEventListener("input", getPredictionsDebounce);
